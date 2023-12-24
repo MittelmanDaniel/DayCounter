32bd3095c37672e0788d91d5043e9ff6198f56a8
Daniel Mittelman <mittelmandaniel@gmail.com>
2023-12-24 17:48:08 -0500

    Initial Commit

---
+++ DayCounter/DayCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayCounterApp.swift
+//  DayCounter
+//
+//  Created by Daniel Mittelman on 12/24/23.
+//
+
+import SwiftUI
+
+@main
+struct DayCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
